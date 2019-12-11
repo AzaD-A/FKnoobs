b8bb48c69c000ecb1e4bf3b0a9543590e000aac5
azad ahmad mhamad <58448316+AzaD-A@users.noreply.github.com>
2019-12-11 21:06:57 +0000

    V 1.0

---
+++ server1.py
@@ -0,0 +1,18 @@
+#created by azad
+#follow me on IG @p8z4
+#FKnoobs V1.0
+import socket
+
+def azad():
+
+        host = '127.0.0.1'
+        port = 2020
+          
+        aza = socket.socket()
+        aza.connect((host,port))
+        
+
+        aza.close
+        
+if __name__ == '__main__':
+    azad()
